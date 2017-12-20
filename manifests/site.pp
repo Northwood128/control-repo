1eb5104ce0b8b2e851cf352577cabcaa0fcb41a1
Northwood128 <agustinrecalde128@gmail.com>
2017-12-20 01:45:00 +0000

    Migrated node definitions from old way to control repo

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,32 +1,43 @@
-## site.pp ##
+node 'node1' {
 
-# This file (/etc/puppetlabs/puppet/manifests/site.pp) is the main entry point
-# used when an agent connects to a master and asks for an updated configuration.
-#
-# Global objects like filebuckets and resource defaults should go in this file,
-# as should the default node definition. (The default node can be omitted
-# if you use the console and don't define any other nodes in site.pp. See
-# http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
-# node definitions.)
+  include base
 
-## Active Configurations ##
+  class { '::tomcat':
+    
+    xms            => '60m',
+    xmx            => '120m',
+    user           => 'tomcat',
+    group          => 'tomcat',
+    service_state  => 'running'
+    
+  }
+  
+  tomcat::deploy {"sysfoo":
+    deploy_url      => 'https://1-114787399-gh.circle-artifacts.com/0/tmp/circle-artifacts.emAMKsh/sysfoo.war',
+    checksum_value  => '0a77630ef8b3e8af4053e8ba21aae01e'
+  }
+}
 
-# Disable filebucket by default for all File resources:
-#https://docs.puppet.com/pe/2015.3/release_notes.html#filebucket-resource-no-longer-created-by-default
-File { backup => false }
+node 'node2' {
 
-# DEFAULT NODE
-# Node definitions in this file are merged with node data from the console. See
-# http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
-# node definitions.
+  include base
 
-# The default node definition matches any node lacking a more specific node
-# definition. If there are no other nodes in this file, classes declared here
-# will be included in every node's catalog, *in addition* to any classes
-# specified in the console for that node.
+  class { '::tomcat':
+    
+    xms            => '70m',
+    xmx            => '130m',
+    user           => 'tomcat',
+    group          => 'tomcat',
+    service_state  => 'running'
+    
+  }
+  
+  tomcat::deploy {"sysfoo":
+    deploy_url      => 'https://1-114787399-gh.circle-artifacts.com/0/tmp/circle-artifacts.emAMKsh/sysfoo.war',
+    checksum_value  => '0a77630ef8b3e8af4053e8ba21aae01e'
+  }
+}
 
 node default {
-  # This is where you can declare classes for all nodes.
-  # Example:
-  #   class { 'my_class': }
+  notify { 'No definitions for this node': }
 }
