d0de837815ba49f3b5b97ade0a5e493b5754986d
Northwood128 <agustinrecalde128@gmail.com>
2017-12-20 02:13:51 +0000

    Changed node definition for dev environment

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,4 @@
-node 'node1' {
+node 'node3' {
 
   include base
 
@@ -6,26 +6,6 @@ node 'node1' {
     
     xms            => '60m',
     xmx            => '120m',
-    user           => 'tomcat',
-    group          => 'tomcat',
-    service_state  => 'running'
-    
-  }
-  
-  tomcat::deploy {"sysfoo":
-    deploy_url      => 'https://1-114787399-gh.circle-artifacts.com/0/tmp/circle-artifacts.emAMKsh/sysfoo.war',
-    checksum_value  => '0a77630ef8b3e8af4053e8ba21aae01e'
-  }
-}
-
-node 'node2' {
-
-  include base
-
-  class { '::tomcat':
-    
-    xms            => '70m',
-    xmx            => '130m',
     user           => 'tomcat',
     group          => 'tomcat',
     service_state  => 'running'
