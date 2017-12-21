68efcfe1be132c9c9a6e07879cd623295e7a464b
Northwood128 <agustinrecalde128@gmail.com>
2017-12-21 00:00:14 +0000

    Changed class definition for include in site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,15 +1,6 @@
 node 'node3' {
 
-  class { '::tomcat':
-    
-    xms            => '60m',
-    xmx            => '120m',
-    user           => 'tomcat',
-    group          => 'tomcat',
-    service_state  => 'running'
-    
-  }
-  
+  include tomcat
   tomcat::deploy {"sysfoo":
     deploy_url      => 'https://1-114787399-gh.circle-artifacts.com/0/tmp/circle-artifacts.emAMKsh/sysfoo.war',
     checksum_value  => '0a77630ef8b3e8af4053e8ba21aae01e'
