add1428b1c50689193a2e69c554a5c061b7ad249
Northwood128 <agustinrecalde128@gmail.com>
2017-12-21 03:03:41 +0000

    More R&P design pattern

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,7 @@
 node 'node3' {
 
-  include tomcat
-  tomcat::deploy {"sysfoo":
-    deploy_url      => 'https://1-114787399-gh.circle-artifacts.com/0/tmp/circle-artifacts.emAMKsh/sysfoo.war',
-    checksum_value  => '0a77630ef8b3e8af4053e8ba21aae01e'
-  }
+  include role::web
+
 }
 
 node default {
