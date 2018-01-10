c22ad956aca9b6c04542fdebd5bdd75afc283ea8
Agustin Recalde <agustinrecalde128@gmail.com>
2018-01-10 11:10:34 +0000

    Draw the rest of the fucking owl

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,15 +1,3 @@
-node 'node1' {
-
-  include role::web
-
-}
-
-node 'node2' {
-
-  include role::web
-
-}
-
 node default {
-  notify { 'No definitions for this node': }
+  include role::elasticsearch
 }
